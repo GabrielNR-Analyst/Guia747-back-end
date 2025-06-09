30904a2d848e61322ca71341343433465ccbf36a
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-06-09 16:40:53 -0300

    Add GlobalMapperConfig interface to centralize MapStruct configuration

---
+++ src/main/java/com/guia747/infrastructure/config/GlobalMapperConfig.java
@@ -0,0 +1,19 @@
+package com.guia747.infrastructure.config;
+
+import org.mapstruct.Builder;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        builder = @Builder(disableBuilder = true)
+)
+public interface GlobalMapperConfig {
+
+}
