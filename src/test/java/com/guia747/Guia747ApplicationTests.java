9ad61dd3c6d478e7273c9d222877450c7d527d79
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-06-09 12:40:25 -0300

    Initial commit

---
+++ src/test/java/com/guia747/Guia747ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guia747;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guia747ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
