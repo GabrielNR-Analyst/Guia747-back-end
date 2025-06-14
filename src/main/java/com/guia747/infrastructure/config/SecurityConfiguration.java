46408d806a6cf4531ceac2f569914505a1408994
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-06-14 20:07:16 -0300

    Configure session management with Redis and update security settings

--- src/main/java/com/guia747/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/com/guia747/infrastructure/config/SecurityConfiguration.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
 public class SecurityConfiguration {
@@ -19,9 +22,22 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/v1/oauth2/loginWithGoogle").permitAll()
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable).httpBasic(AbstractHttpConfigurer::disable);
+
+        http.securityContext(context -> context.securityContextRepository(securityContextRepository()));
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::migrateSession)
+                .maximumSessions(10)
+                .maxSessionsPreventsLogin(false)
+        );
+
         return http.build();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
     }
 
     @Bean
