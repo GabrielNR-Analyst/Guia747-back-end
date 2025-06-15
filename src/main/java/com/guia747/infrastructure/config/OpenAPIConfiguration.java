2fef21067a28d50175d18b1ce74db59c76a2bdfb
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-06-15 13:26:37 -0300

    add OpenAPIConfiguration

---
+++ src/main/java/com/guia747/infrastructure/config/OpenAPIConfiguration.java
@@ -0,0 +1,31 @@
+package com.guia747.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Guia747 API",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local Development Server")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER,
+        description = "JWT authentication using a Bearer token. Example: 'Bearer {token}'"
+)
+public class OpenAPIConfiguration {
+
+}
